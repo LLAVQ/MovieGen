8ff7b9214457e29913bae27f19540315efe8b229
LLAVQ <qqname21@gmail.com>
2025-05-06 19:31:58 +0000

    BASE

---
+++ frontend/MovieGen/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      message: 'Loading...'
+    }
+  },
+  mounted() {
+    axios.get('http://localhost:8000/api/hello')
+      .then(response => {
+        this.message = response.data.message
+      })
+      .catch(error => {
+        this.message = 'Failed to load message from backend.'
+        console.error(error)
+      })
+  }
+}
+</script>
+
+<style>
+h1 {
+  font-family: sans-serif;
+}
+</style>
